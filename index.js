7a84196a2c423fe69f44e3c4a668391eb307c16b
Ollie Buck <git@olliebuck.com>
2016-05-20 16:33:21 +0100

    more stuff

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var GitHubApi = require("github");
+
+var port = process.env.PORT || 4646;
+
+var github = new GitHubApi({
+  version: "3.0.0",
+  debug: true,
+  timeout: 10000,
+  headers: {
+    "user-agent": "Persistent-PR"
+  }
+});
+
+github.authenticate({
+  type: 'oauth',
+  token: process.env.GITHUB_TOKEN
+});
+
+var sourceBranch = process.env.SOURCE_BRANCH;
+var targetBranch = process.env.TARGET_BRANCH;
+
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/hook', function (req, res) {
   console.log(req.body);
+
+  if (req.body.ref === 'refs/heads/' + sourceBranch) {
+    github.repos.getContent({
+      user: process.env.REPO_OWNER,
+      repo: process.env.REPO_NAME,
+      path: '/.github/DEPLOYMENT_PULL_REQUEST_TEMPLATE.md',
+      ref: sourceBranch
+    }, function(err, fileContents) {
+      var body = (err || !fileContents) ? '' : new Buffer(fileContents.content, 'base64').toString('utf8');
+
+      // This will fail if there are not differences or if one is already open,
+      // keeping things simple by always attempting it and ignoring errors.
+      github.pullRequests.create({
+        user: process.env.REPO_OWNER,
+        repo: process.env.REPO_NAME,
+        title: sourceBranch + ' -> ' + targetBranch,
+        head: sourceBranch,
+        base: targetBranch,
+        body: body
+      }, function(err) {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  }
+
   res.end();
 });
 
-app.listen(4646, function () {
-  console.log('Example app listening on port 4646!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
